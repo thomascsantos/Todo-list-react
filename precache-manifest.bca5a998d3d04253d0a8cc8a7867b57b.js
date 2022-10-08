ea07e73a5876f709f2f27756ad3ace265e75349f
thomas santos <thomascsantos2@gmail.com>
2022-10-08 13:27:16 -0300

    commit teste

---
+++ precache-manifest.bca5a998d3d04253d0a8cc8a7867b57b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f1515c02dad85538acf",
+    "url": "./static/css/main.c20a9769.chunk.css"
+  },
+  {
+    "revision": "7f1515c02dad85538acf",
+    "url": "./static/js/main.6f5b6edc.chunk.js"
+  },
+  {
+    "revision": "b631bb0608d7b79f0779",
+    "url": "./static/js/runtime~main.58237724.js"
+  },
+  {
+    "revision": "8c3a309cb0504acc461e",
+    "url": "./static/js/2.cbbbb7c3.chunk.js"
+  },
+  {
+    "revision": "1e38ecc495746bd18357",
+    "url": "./static/js/3.12bf67e3.chunk.js"
+  },
+  {
+    "revision": "3feef6c8316af5ff1ff5cb98707046e1",
+    "url": "./index.html"
+  }
+];
